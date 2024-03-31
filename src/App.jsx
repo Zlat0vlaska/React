50e078298b78817855d6d3c03ccb751541d40faf
Zlat0vlaska <nesterov0303@mail.ru>
2024-03-31 16:12:09 +0300

    On branch typescript  Changes to be committed: 	renamed:    src/App.js -> src/App.jsx 	renamed:    src/index.js -> src/index.tsx 	new file:   src/pages/Home.jsx 	modified:   src/pages/UserPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import Header from './Components/Header';
 import './styles/App.css';
+import Home from './pages/Home';
 import Blog from "./pages/Blog";
 import UserPage from "./pages/UserPage";
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout';
-import Likes from './Components/Likes';
+
+
 function App() {
 
 
@@ -14,14 +14,20 @@ function App() {
       <div className='.Header'>
         <Routes>
           <Route path="/" element={<Layout />} >
-            <Route index element={<Likes />} />
+            <Route index element={<Home/>} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/users" element={<UserPage />} />
           </Route>
-
         </Routes>
 
       </div>
+
+      <form>
+        <input type='text'/>
+        <input type='number'/>
+        
+        <button>Отправить</button>
+      </form>
     </div>
   );
 }
